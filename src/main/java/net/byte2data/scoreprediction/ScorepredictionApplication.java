05f6ad335572b744d37975dfb1334f36838941ba
Ayberk Magden <ayberkmagden@gmail.com>
2022-09-22 10:44:01 +0300

    prediction commit

---
+++ src/main/java/net/byte2data/scoreprediction/ScorepredictionApplication.java
@@ -0,0 +1,13 @@
+package net.byte2data.scoreprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScorepredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScorepredictionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/byte2data/scoreprediction/ScorepredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package net.byte2data.scoreprediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScorepredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
